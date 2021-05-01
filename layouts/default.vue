e6f019cefd839865b08c8a53ebe9393ad70e7e6a
Izabela Magiera <hello@izabelamagiera.com>
2021-05-01 10:22:29 +0100

    initial pload

---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.objektiv {
+  font-family: objektiv-mk1, sans-serif;
+}
+.tenez {
+  font-family: tenez, sans-serif;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="w-full">
+    <div>
+      <h2
+        class="objektiv light text-2xl text-center color-base p-8 py-14 uppercase tracking-wider"
+      >
+        izabela magiera
+      </h2>
+    </div>
+    <div class="pr-24">
+      <div class="w-full relative bg-tan pl-24 radius-br">
+        <div class="container mx-auto max-w-3xl px-8 py-28">
+          <p class="objektiv text-xs text-center regular tracking-wider mb-2">
+            FULL WEBSITE COMING SOON
+          </p>
+          <p class="tenez text-center text-lrg">
+            I code & build user centred websites and ecommerce shops with a
+            strong focus on flawless functionality and improved SEO
+          </p>
+          <div class="flex flex-row justify-center mx-auto max-w-lg mt-4">
+            <div class="my-2 mx-4">
+              <img src="logo-webflow-p-500.png" alt="" />
+            </div>
+            <div class="my-2 mx-4">
+              <img src="logo-squarespace-p-500.png" alt="" />
+            </div>
+            <div class="my-2 mx-4">
+              <img src="logo-shopify-p-500.png" alt="" />
+            </div>
+            <div class="my-2 mx-4">
+              <img src="logo-wordpress-p-500.png" alt="" />
+            </div>
+          </div>
+          <div class="absolute bottom-0 right-0">
+            <imcrtv-logo class="absolute bottom-0" />
+            <img class="logo-spin" src="imcrtv-logo-txt.svg" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scope>
+.logo-spin {
+  width: 100px;
+  height: auto;
+}
+</style>
